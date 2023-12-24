5fee9ec7f1aa46e10134884dbcbbcb19f156363d
Palash Dhavle <palashdhavle15@gmail.com>
2023-12-25 00:00:01 +0530

    feat: updated tailwind config to add heartbeat animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ export default {
       fontFamily: {
         roboto: 'Roboto', // Adds a new `font-inter` class
         pacifico: 'Pacifico' // Adds a new `font-inter` class
+      },
+      animation: {
+        heartbeat: 'heartbeat 1s infinite',
+      },
+      keyframes: {
+        heartbeat: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.1)' },
+          '60%': { transform: 'scale(1.2)' },
+        }
       }
+
     },
   },
   plugins: [],

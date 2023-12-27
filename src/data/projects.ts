3fff751f6d4754517fe7e296493ff342fc3c3693
Palash Dhavle <palashdhavle15@gmail.com>
2023-12-27 23:45:40 +0530

    chore: updated project list for testing

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,5 @@
 interface Project {
+    title: string
     name: string
     desc:string
     techStack: string[]
@@ -7,6 +8,23 @@ interface Project {
 
 const projects: Project[] = [
 	{
+		title: "Auto Learn",
+		name: "autolearn",
+		desc: "AutoLearn is an innovative platform designed to revolutionize your learning experience. Harnessing the power of AI, AutoLearn generates structured educational content, breaks down complex topics into manageable chunks, and automatically creates quizzes to reinforce your understanding. Additionally, it helps you plan your learning journey by automatically scheduling your study timetable based on your availability and desired daily study hours.",
+		techStack: [
+			"python.png",
+			"javascript.png",
+			"django.png",
+			"react.png",
+			"react-router.svg",
+			"radix.png",
+			"tailwind.png",
+			"PaLM.png",
+		],
+		images: ["login.png", "dashboard.png", "main_bg.jpg"],
+	},
+	{
+		title: "Auto Learn2",
 		name: "autolearn",
 		desc: "AutoLearn is an innovative platform designed to revolutionize your learning experience. Harnessing the power of AI, AutoLearn generates structured educational content, breaks down complex topics into manageable chunks, and automatically creates quizzes to reinforce your understanding. Additionally, it helps you plan your learning journey by automatically scheduling your study timetable based on your availability and desired daily study hours.",
 		techStack: [

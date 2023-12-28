42bd9fe0a5a999aa6b9c53c4c8cc5d8115488a4e
Palash Dhavle <palashdhavle15@gmail.com>
2023-12-29 02:01:57 +0530

    feat: added experiece compnent

--- src/components/Exprience.tsx
+++ src/components/Exprience.tsx
@@ -1,17 +1,14 @@
 import { ArrowBigDownDash } from "lucide-react";
 import TechTile from "./TechTile";
+import experience from "../data/exprience";
+import { useState } from "react";
 
 const Exprience: React.FC = () => {
-    const techStack = [
-        "python.png",
-        "javascript.png",
-        "django.png",
-        "react.png",
-        "react-router.svg",
-        "radix.png",
-        "tailwind.png",
-        "PaLM.png"
-    ]
+	const [exp, setexp] = useState(experience.backendEgineer);
+    const [active, setactive] = useState(experience.backendEgineer.title);
+	const changeActive = (name: string) => {
+		setactive(name);
+	};
 	return (
 		<>
 			<div className="px-5">
@@ -35,21 +32,41 @@ const Exprience: React.FC = () => {
 							className="flex flex-col justify-around h-96 font-roboto"
 							style={{ height: "30rem" }}
 						>
-							<div className="cursor-pointer text-orange-500 text-3xl hover:scale-105">
-								2021
-							</div>
-							<div className="cursor-pointer text-3xl hover:scale-105">
-								2022
-							</div>
-							<div className="cursor-pointer text-3xl hover:scale-105">
-								2023
-							</div>
+							{Object.entries(experience).map(([key, value]) => {
+								return (
+									<div
+										key={key}
+										className={`cursor-pointer ${active === value.title && "text-orange-500"} text-3xl hover:scale-105`}
+										onClick={() => {
+                                            changeActive(value.title)
+											setexp(experience[key]);
+										}}
+										onKeyDown={() => {
+                                            changeActive(value.title)
+											setexp(experience[key]);
+										}}
+									>
+										{value.year}
+									</div>
+								);
+							})}
 						</div>
 						<div
 							className="flex flex-col justify-around h-96 border-s-4 rounded-sm w-28"
 							style={{ height: "30rem" }}
 						>
-							<div className="h-1 bg-white rounded-sm">
+                            {Object.entries(experience).map(([key, value]) => {
+								return (
+									<div
+										key={key}
+										className="h-1 bg-white rounded-sm"
+
+									>
+										<div className={`w-5 h-5 relative bottom-2 right-3 rounded-full ${active === value.title? "bg-orange-500":"bg-white"} ring-2 ring-white`} />
+									</div>
+								);
+							})}
+							{/* <div className="h-1 bg-white rounded-sm">
 								<div className="w-5 h-5 relative bottom-2 right-3 rounded-full bg-orange-500 ring-2 ring-white" />
 							</div>
 							<div className="h-1 bg-white rounded-sm">
@@ -57,21 +74,30 @@ const Exprience: React.FC = () => {
 							</div>
 							<div className="h-1 bg-white rounded-sm">
 								<div className="w-5 h-5 relative bottom-2 right-3 rounded-full bg-white" />
-							</div>
+							</div> */}
 						</div>
 						<div
 							className="flex flex-col justify-around h-96 font-roboto"
 							style={{ height: "30rem" }}
 						>
-							<div className="cursor-pointer text-orange-500 text-3xl hover:scale-105">
-								Backend Engineer
-							</div>
-							<div className="cursor-pointer text-3xl hover:scale-105">
-								FullStack Engineer
-							</div>
-							<div className="cursor-pointer text-3xl hover:scale-105">
-								SDE 2
-							</div>
+							{Object.entries(experience).map(([key, value]) => {
+								return (
+									<div
+										key={key}
+										className={`cursor-pointer ${active === value.title && "text-orange-500"} text-3xl hover:scale-105`}
+										onClick={() => {
+                                            changeActive(value.title)
+											setexp(experience[key]);
+										}}
+										onKeyDown={() => {
+                                            changeActive(value.title)
+											setexp(experience[key]);
+										}}
+									>
+										{value.title}
+									</div>
+								);
+							})}
 						</div>
 					</div>
 					<div className="basis-1/2">
@@ -90,45 +116,32 @@ const Exprience: React.FC = () => {
 									width: "35rem",
 								}}
 							>
-								<div className="font-roboto text-4xl mt-10">
-									Backend Engineer
+								<div className="font-roboto text-4xl mt-10">{exp.title}</div>
+								<div className="flex justify-between ms-1">
+									<p className="font-roboto text-lg">{exp.company}</p>
+									<p className="text-lg">{`${exp.duration[0]}-${exp.duration[1]}`}</p>
 								</div>
-								<div className="flex justify-between mb-2 ms-1">
-									<p className="font-roboto text-lg">BlueCognition</p>
-									<p className="text-lg">2021-2022</p>
+							</div>
+							<div className="overflow-auto h-52 mt-3">
+								<div className=" ms-9">
+									<ul className="list-disc text-md">
+										{exp.work.map((item, index) => {
+											return (
+												<li key={`${index}${item.slice(0, 5)}`}>{item}</li>
+											);
+										})}
+									</ul>
 								</div>
-								<div className="overflow-auto h-52 mt-8">
-									<div className=" ms-9">
-										<ul className="list-disc text-md">
-											<li>
-												Conceptualized and crafted a highly efficient Electron
-												JS application for digitizing and centralizing employee
-												data.
-											</li>
-											<li>
-												Enabled seamless tracking of business operations,
-												enhancing data accessibility and management.
-											</li>
-											<li>
-												chieved a significant 20% enhancement in workflow
-												efficiency and data accuracy metrics.
-											</li>
-											<li>
-												Streamlined data tracking processes, resulting in
-												improved operational efficiency and increased accuracy
-												in business operations.
-											</li>
-										</ul>
-									</div>
+							</div>
+							<div className="mt-3 ms-5">
+								<p className="font-roboto text-lg">TechStack</p>
+								<div className="flex flex-wrap gap-5 w-full overflow-y-auto h-14 mt-2">
+									{exp.techStack.map((tech) => {
+										return (
+											<TechTile tech={tech} key={tech} dimensions={"11"} />
+										);
+									})}
 								</div>
-                                <div className="mt-2 ms-2">
-                                    <p className="font-roboto text-lg">TechStack</p>
-                                    <div className="flex flex-wrap gap-5 w-full overflow-y-auto h-14 mt-2">
-											{techStack.map((tech) => {
-												return <TechTile tech={tech} key={tech} dimensions={"11"} />;
-											})}
-										</div>
-                                </div>
 							</div>
 						</div>
 					</div>

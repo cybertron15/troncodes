33b76fe416eb75c2f92e2a34c50692b3287986af
Palash Dhavle <palashdhavle15@gmail.com>
2023-12-29 02:03:20 +0530

    chore: spelling correction in navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,11 @@
 import { useState} from "react";
 
 const Navbar: React.FC = () => {
-	const navItems = ['home','skills','blog','projects','exprience','contact']
+	const navItems = ['home','skills','blog','projects','experience','contact']
 	const [active, setactive] = useState('home')
 	const changeActive = (event: React.MouseEvent<HTMLLIElement> | React.KeyboardEvent<HTMLLIElement>)=> {
 		const target = event.currentTarget
 		setactive(target.innerText)
-		
 	}
 	return (
 		<nav className="flex justify-between mx-5 mt-5">

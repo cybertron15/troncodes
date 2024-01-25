69a21edc5fec3b6f29d6b7514c268fa5d03a3003
Palash Dhavle <palashdhavle15@gmail.com>
2024-01-25 23:57:19 +0530

    feat: modified hero 3

--- src/components/Hero3.tsx
+++ src/components/Hero3.tsx
@@ -1,9 +1,21 @@
-
-const Hero3: React.FC = () => {
+const Hero3: React.FC = ({controller,c,c2}) => {
 	return (
-		<div className="text-center fixed z-20 w-full mt-24">
+		<div className="text-center fixed z-20 w-full mt-24 h-96">
 			<div className="font-roboto text-5xl">
 				along with debts I have these skills
+			</div>
+			{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+			<div
+				className="border border-red-400 h-32 w-32 mt-64"
+				style={{ transform: `translateY('100px')`,background:"white" }}
+				onClick={() => {
+					console.log('clicked');
+					controller(!c)
+					console.log(c,c2);
+				}
+				}
+			>
+
 			</div>
 		</div>
 	);

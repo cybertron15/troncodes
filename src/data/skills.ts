894bb1372d908e2ea2b192313e76ed22d53cb827
Palash Dhavle <palashdhavle15@gmail.com>
2024-01-28 19:48:12 +0530

    feat: added skill data file

---
+++ src/data/skills.ts
@@ -0,0 +1,54 @@
+interface Skills {
+    name: string
+    img: string
+}
+
+const skills: Skills[] = [
+    {
+        name: "Python",
+        img: "python.png"
+    },
+    {
+        name: "Django",
+        img: "django.png"
+    },
+    // {
+    //     name: "Flask",
+    //     img: "flask.png"
+    // },
+    {
+        name: "Fast API",
+        img: "FastAPI.jpg"
+    },
+    {
+        name: "JavaScript",
+        img: "javascript.png"
+    },
+    {
+        name: "TypeScript",
+        img: "ts.png"
+    },
+    {
+        name: "React",
+        img: "react.png"
+    },
+    {
+        name: "Tailwind",
+        img: "tailwind.png"
+    },
+    {
+        name: "Radix",
+        img: "radix.png"
+    },
+    {
+        name: "PaLM",
+        img: "PaLM.png"
+    }, 
+    {
+        name: "Mongoose JS",
+        img: "mongoosejs.png"
+    }
+
+]
+
+export default skills

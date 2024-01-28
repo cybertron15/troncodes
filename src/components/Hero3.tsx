27ccbeb4a71095aa8d8651d72c58b3804816af27
Palash Dhavle <palashdhavle15@gmail.com>
2024-01-28 19:47:49 +0530

    feat: added scroll carousal

--- src/components/Hero3.tsx
+++ src/components/Hero3.tsx
@@ -1,22 +1,36 @@
-const Hero3: React.FC = ({controller,c,c2}) => {
+import { useRef, useEffect } from "react";
+import ScrollCarousel from "scroll-carousel-react";
+import skills from "../data/skills";
+import TechTile from "./TechTile";
+
+const Hero3: React.FC = () => {
 	return (
-		<div className="text-center fixed z-20 w-full mt-24 h-96">
+		<div className="text-center fixed z-20 w-full mt-24">
 			<div className="font-roboto text-5xl">
 				along with debts I have these skills
 			</div>
-			{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-			<div
-				className="border border-red-400 h-32 w-32 mt-64"
-				style={{ transform: `translateY('100px')`,background:"white" }}
-				onClick={() => {
-					console.log('clicked');
-					controller(!c)
-					console.log(c,c2);
-				}
-				}
+			<ScrollCarousel
+				autoplay
+				autoplaySpeed={8}
+				speed={7}
+				onReady={() => console.log("I am ready")}
 			>
-
-			</div>
+				{skills.map((item) => (
+					<div
+						key={item.name}
+						className="rounded h-48 w-40 mx-7 flex flex-col justify-center items-center mt-10"
+					>
+						<img
+							src={`images/techstack/${item.img}`}
+							alt="icon"
+							className="object-contain rounded-xl w-32 h-32 hover:scale-105 transition-transform duration-300 transform cursor-pointer hover:ring-1 ring-orange-500"
+						/>
+						<div className="mt-2 text-2xl">
+							{item.name}
+						</div>
+					</div>
+				))}
+			</ScrollCarousel>
 		</div>
 	);
 };

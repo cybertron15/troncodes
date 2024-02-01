8a1c4e91fd60d2c573167eff5615cad9cc91dfac
Palash Dhavle <palashdhavle15@gmail.com>
2024-02-02 01:06:31 +0530

    feat: integrated formspree to directly send me email notification when someone contacts me

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+// using formspree to directly mail me messages 
+// https://formspree.io/
 
 const Contact: React.FC = () => {
+	const formspreeSecret = import.meta.env.VITE_FORMSPREE_SECRET;
+	const [state, handleSubmit] = useForm(formspreeSecret);
 	return (
 		<div className="text-center mt-10 mb-36">
 			<div className="font-roboto text-5xl">Contact</div>
@@ -23,32 +28,51 @@ const Contact: React.FC = () => {
 			<div className="font-roboto text-xl">
 				can fill this form to reach out to me.
 			</div>
-			<div className="flex flex-col w-full gap-3 justify-center items-center mt-5">
-				<input type="text" placeholder="Name" className="p-2 rounded-md w-96 focus:outline-orange-500 text-black" />
-				<input
-					type="email"
-					placeholder="Email"
-					className="p-2 rounded-md w-96 focus:outline-orange-500 text-black"
-				/>
-				<textarea
-					id="w3review"
-					name="w3review"
-					placeholder="Message..."
-					className="p-2 rounded-md w-96 h-32 focus:outline-orange-500 text-black"
-				/>
-				<input
-					type="button"
-					value="Send"
-					className="bg-orange-500 w-24 p-2 rounded-md text-xl cursor-pointer hover:scale-105"
-				/>
-				<div className="flex" style={{marginLeft:"35rem"}}>
-					<img src="images/arrow.png" alt="arrow" />
-					<div className="mb-8 relative bottom-20 text-3xl font-caveat -rotate-12">
-						<p>You can also contact me </p>
-						<p>through my socials </p>
+			<form onSubmit={handleSubmit}>
+				<div className="flex flex-col w-full gap-3 justify-center items-center mt-5">
+					<input
+						type="text"
+						placeholder="Name"
+						name="name"
+						id="name"
+						className="p-2 rounded-md w-96 focus:outline-orange-500 text-black"
+					/>
+					<ValidationError prefix="Name" field="text" errors={state.errors} />
+					<input
+						type="email"
+						placeholder="Email"
+						name="email"
+						id="email"
+						className="p-2 rounded-md w-96 focus:outline-orange-500 text-black"
+					/>
+					<ValidationError prefix="Email" field="email" errors={state.errors} />
+					<textarea
+						id="message"
+						name="message"
+						placeholder="Message..."
+						className="p-2 rounded-md w-96 h-32 focus:outline-orange-500 text-black"
+					/>
+					<ValidationError
+						prefix="Message"
+						field="text"
+						errors={state.errors}
+					/>
+					<input
+						type="submit"
+						value={state.succeeded ? "Message sent" : "Send"}
+						disabled={state.succeeded || state.submitting}
+						className={`${(state.succeeded || state.submitting)?"bg-orange-300":"bg-orange-500 hover:scale-105 cursor-pointer"} w-fit p-2 px-4 rounded-md text-xl`}
+					/>
+
+					<div className="flex" style={{ marginLeft: "35rem" }}>
+						<img src="images/arrow.png" alt="arrow" />
+						<div className="mb-8 relative bottom-20 text-3xl font-caveat -rotate-12">
+							<p>You can also contact me </p>
+							<p>through my socials </p>
+						</div>
 					</div>
 				</div>
-			</div>
+			</form>
 		</div>
 	);
 };

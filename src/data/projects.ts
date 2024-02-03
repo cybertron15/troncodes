a710c6b9ab824ebc9fc089d7603734739971b92c
Palash Dhavle <palashdhavle15@gmail.com>
2024-02-03 23:56:16 +0530

    chore: updated projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -24,18 +24,13 @@ const projects: Project[] = [
 		images: ["login.png", "dashboard.png", "main_bg.jpg"],
 	},
 	{
-		title: "Auto Learn2",
-		name: "autolearn",
-		desc: "AutoLearn is an innovative platform designed to revolutionize your learning experience. Harnessing the power of AI, AutoLearn generates structured educational content, breaks down complex topics into manageable chunks, and automatically creates quizzes to reinforce your understanding. Additionally, it helps you plan your learning journey by automatically scheduling your study timetable based on your availability and desired daily study hours.",
+		title: "Air Brush",
+		name: "airbrush",
+		desc: "Effortlessly draw on-screen using gestures, naturally control the UI with hand movements, and experience seamless integration of gestures with the interface.",
 		techStack: [
 			"python.png",
-			"javascript.png",
-			"django.png",
-			"react.png",
-			"react-router.svg",
-			"radix.png",
-			"tailwind.png",
-			"PaLM.png",
+			"mediapipe.png",
+			"opencv.png"
 		],
 		images: ["login.png", "dashboard.png", "main_bg.jpg"],
 	},

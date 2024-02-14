8472cba6208e2fe23899328163da2816fdffdba3
Palash Dhavle <palashdhavle15@gmail.com>
2024-02-14 22:18:03 +0530

    feat: added reponsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
+import { Menu, X} from "lucide-react";
 import React, { useState} from "react";
+
+
 interface NavbarProps {
 	navbarController: (navigateTo: string) => void;
   };
@@ -6,6 +9,7 @@ interface NavbarProps {
 const Navbar:  React.FC<NavbarProps>  = ({ navbarController }) => {
 	const navItems = ['home','skills','blog','projects','experience','contact']
 	const [active, setactive] = useState('home')
+	const [showNavbar, setshowNavbar] = useState(true)
 	const changeActive = (event: React.MouseEvent<HTMLLIElement> | React.KeyboardEvent<HTMLLIElement>)=> {
 		const target = event.currentTarget
 		const section = target.innerText
@@ -14,20 +18,30 @@ const Navbar:  React.FC<NavbarProps>  = ({ navbarController }) => {
 		
 	}
 	return (
-		<nav className="flex justify-between mx-5 pt-5">
-			<div>
+		<nav className="md:flex md:justify-between md:px-5 pt-5">
+			<div className={`flex w-full justify-between ${showNavbar && "bg-black"} px-5`}>
 				{/* this component is placed outisde the parralax component of react-spring which i am using in app component
 				the parrallax component has an image inside which i want to scroll up faster as I scroll but the problem was when the
 				image reached the top of the page the below div with TronCodes was hiding behind the image and for some weird reason
 				if I add transform class to it does not hide and works fine IDK how it works but its working */}
-				<div className="font-pacifico text-3xl cursor-pointer transform">TronCodes</div>
+				<div className={"font-pacifico text-3xl cursor-pointer transform"}>TronCodes</div>
+				<button className="md:hidden" type="button" onClick={()=>{
+					setshowNavbar(!showNavbar)
+					console.log(showNavbar);
+					
+				}}>
+					{showNavbar? <X/>:
+					<Menu />}
+				</button>
 			</div>
-			<ul className="flex gap-10 text-xl font-roboto mt-auto">
+			{showNavbar && 
+			<ul className="md:flex-row md:gap-10 md:bg-transparent md:pt-0 text-xl font-roboto mt-auto flex flex-col gap-8 min-h-fit bg-black pt-5 ps-6">
 				{navItems.map((item:string)=>{
-					return <li key={item} onClick={changeActive} onKeyDown={(event)=>changeActive(event)} className={`hover:text-orange-500 hover:scale-110 cursor-pointer transition-transform duration-300 transform ${item===active && "text-orange-500"}`}>{item}</li>
+					return <li key={item} onClick={changeActive} onKeyDown={(event)=>changeActive(event)} className={`hover:text-orange-500 hover:md:scale-110 cursor-pointer transition-transform duration-300 transform ${item===active && "text-orange-500"}`}>{item}</li>
 				})}
 			
 			</ul>
+			}
 		</nav>
 	);
 };

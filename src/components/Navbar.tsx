cd87a9fa58dfc57209ecca34284208d99eb4d980
Palash Dhavle <palashdhavle15@gmail.com>
2024-02-14 22:20:31 +0530

    fix: fix minor bug in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar:  React.FC<NavbarProps>  = ({ navbarController }) => {
 		
 	}
 	return (
-		<nav className="md:flex md:justify-between md:px-5 pt-5">
+		<nav className={`md:flex md:justify-between md:px-5 ${showNavbar && "bg-black"} pt-5` }>
 			<div className={`flex w-full justify-between ${showNavbar && "bg-black"} px-5`}>
 				{/* this component is placed outisde the parralax component of react-spring which i am using in app component
 				the parrallax component has an image inside which i want to scroll up faster as I scroll but the problem was when the

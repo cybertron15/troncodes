571d01eb300958467404a4427f7d5ee4ccaf9546
Palash Dhavle <palashdhavle15@gmail.com>
2024-02-19 16:52:36 +0530

    chore: refactored tech tile

--- src/components/TechTile.tsx
+++ src/components/TechTile.tsx
@@ -2,20 +2,24 @@ import React from "react";
 interface ImageDisplayProps {
 	tech: string;
 	dimensions: string;
+	img:string;
+	link: string;
 }
-const TechTile: React.FC<ImageDisplayProps> = ({ tech, dimensions}) => {
+const TechTile: React.FC<ImageDisplayProps> = ({ tech,img, dimensions,link}) => {
 	return (
-		<div
+		<a
 			title={tech.slice(0, -4)}
 			className={`w-${dimensions} h-${dimensions} flex justify-center items-center rounded-lg hover:scale-105 transition-transform duration-300 transform cursor-pointer`}
 			// className={`flex justify-center items-center rounded-lg hover:scale-105 transition-transform duration-300 transform cursor-pointer w-11 h-11`}
+			href={link}
+			target="blank"
 		>
 			<img
 				className="object-contain rounded-lg"
-				src={`images/techstack/${tech}`}
+				src={`images/techstack/${img}`}
 				alt="icon"
 			/>
-		</div>
+		</a>
 	);
 };
 

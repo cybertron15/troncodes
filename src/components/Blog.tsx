bd9a12ce4e1f9560d65e455a5df4f78b57a8b2e0
Palash Dhavle <palashdhavle15@gmail.com>
2024-02-20 01:26:13 +0530

    chore: changed blog heading

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import BlogItem from "./BlogItem";
-import stringShortner from "../utils/stringShortner";
 import blogs from "../data/blogs";
 import { ArrowUpRight } from "lucide-react";
 const Blog: React.FC = () => {
 	return (
 		<div className="px-5">
 			<div className="md:flex md:justify-between lg:mt-10 md:mt-8 mt-5">
-				<div className="font-roboto md:text-3xl lg:text-5xl sm:text-5xl text-4xl">I write sometimes...</div>
+				<div className="font-roboto md:text-3xl lg:text-5xl sm:text-5xl text-4xl">I Enjoy Writing</div>
 				<a
 					href="https://blog.troncodes.com/"
 					target="_blank"
@@ -25,17 +24,14 @@ const Blog: React.FC = () => {
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 space-y-4 md:space-y-0 pt-8 pb-20 w-full">
 				{blogs.map((blog) => {
 					return (
-						<div className="">
 							<BlogItem
 							key={blog.title}
 							image={blog.img}
-							// title={stringShortner(blog.title, 38)}
 							title={blog.title}
-							// desc={stringShortner(blog.desc, 135)}
 							desc={blog.desc}
 							link={blog.link}
 							/>
-						</div>
+					
 					);
 				})}
 			</div>

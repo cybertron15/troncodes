9f1382693528bf7697839c05e6cf704708a326e1
Palash Dhavle <palashdhavle15@gmail.com>
2024-02-19 16:25:08 +0530

    feat: made blog item responsive

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -8,7 +8,7 @@ interface BlogItemProps {
 
 const BlogItem: React.FC<BlogItemProps> = (props) => {
 	return (
-		<div className="col-span-1 h-64 transition-transform duration-300 transform hover:scale-105 cursor-pointer hover:ring-2 ring-orange-500">
+		<div className="h-64 transition-transform duration-300 transform hover:scale-105 cursor-pointer hover:ring-2 ring-orange-500">
 			<a href={props.link} target="_blank" rel="noreferrer">
 				<div
 					className="bg-cover bg-center h-full flex flex-col justify-between"
@@ -20,8 +20,8 @@ const BlogItem: React.FC<BlogItemProps> = (props) => {
 						{/* this div is used so i can push the other div at the bottom using flex */}
 					</div>
 					<div className="px-2 bg-gradient-to-t from-black to-transparent opacity-100">
-						<div className="font-roboto text-2xl">{props.title}</div>
-						<div className="font-roboto text-sm">{props.desc}</div>
+						<div className="font-roboto md:text-2xl sm:text-4xl text-xl truncate ...">{props.title}</div>
+						<div className="font-roboto md:text-sm text-sm overflow-hidden text-ellipsis h-10">{props.desc}</div>
 					</div>
 				</div>
 			</a>

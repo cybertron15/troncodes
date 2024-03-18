d74ce0eae3e16ddd4edcc5e642762b2d93653aa1
Palash Dhavle <palashdhavle15@gmail.com>
2024-03-18 18:07:57 +0530

    update: updated tech tile

--- src/components/TechTile.tsx
+++ src/components/TechTile.tsx
@@ -9,13 +9,12 @@ const TechTile: React.FC<ImageDisplayProps> = ({ tech,img, dimensions,link}) => 
 	return (
 		<a
 			title={tech.slice(0, -4)}
-			className={`w-${dimensions} h-${dimensions} flex justify-center items-center rounded-lg hover:scale-105 transition-transform duration-300 transform cursor-pointer`}
-			// className={`flex justify-center items-center rounded-lg hover:scale-105 transition-transform duration-300 transform cursor-pointer w-11 h-11`}
+			className={'rounded-lg hover:scale-105 transition-transform duration-300 transform cursor-pointer flex-shrink-0'}
 			href={link}
 			target="blank"
 		>
 			<img
-				className="object-contain rounded-lg"
+				className={`w-${dimensions} h-${dimensions} object-contain rounded-lg`}
 				src={`images/techstack/${img}`}
 				alt="icon"
 			/>

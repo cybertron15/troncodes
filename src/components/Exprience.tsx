5cf9970a66a36b8e1dcebdd344a946794457b7cc
Palash Dhavle <palashdhavle15@gmail.com>
2024-03-21 18:13:06 +0530

    update: made exprience column responsive

--- src/components/Exprience.tsx
+++ src/components/Exprience.tsx
@@ -12,8 +12,8 @@ const Exprience: React.FC = () => {
 	return (
 		<>
 			<div className="px-5">
-				<div className="flex justify-between">
-					<div className="font-roboto text-5xl ">I have been to places</div>
+				<div className="md:flex md:justify-between">
+					<div className="font-roboto md:text-3xl lg:text-5xl sm:text-5xl text-4xl">I have been to places</div>
 					<a
 						href="files/PalashDhavleResume.pdf"
 						target="_blank"
@@ -26,17 +26,16 @@ const Exprience: React.FC = () => {
 						</span>
 					</a>
 				</div>
-				<div className="flex mt-6 ps-24">
-					<div className="basis-1/2 flex gap-6">
+				<div className="flex flex-col lg:flex-row mt-6 lg:ps-24 gap-2 xl:gap-0">
+					<div className="basis-1/2 lg:flex gap-6">
 						<div
-							className="flex flex-col justify-around h-96 font-roboto"
-							style={{ height: "30rem" }}
+							className="flex lg:flex-col justify-around lg:h-[30rem] font-roboto"
 						>
 							{Object.entries(experience).map(([key, value]) => {
 								return (
 									<div
 										key={key}
-										className={`cursor-pointer ${active === value.title && "text-orange-500"} text-3xl hover:scale-105`}
+										className={`cursor-pointer ${active === value.title && "text-orange-500"} md:text-xl lg:text-2xl xl:text-3xl hover:scale-105`}
 										onClick={() => {
                                             changeActive(value.title)
 											setexp(experience[key]);
@@ -52,17 +51,16 @@ const Exprience: React.FC = () => {
 							})}
 						</div>
 						<div
-							className="flex flex-col justify-around h-96 border-s-4 rounded-sm w-28"
-							style={{ height: "30rem" }}
+							className="flex lg:flex-col justify-around lg:h-[30rem] h-[2rem] lg:border-s-4 border-t-2 md:border-t-4 lg:border-t-0 rounded-sm mt-3 lg:mt-0"
 						>
                             {Object.entries(experience).map(([key, value]) => {
 								return (
 									<div
 										key={key}
-										className="h-1 bg-white rounded-sm"
+										className="lg:h-1 w-[2px] md:w-1 lg:w-36 bg-white rounded-sm"
 
 									>
-										<div className={`w-5 h-5 relative bottom-2 right-3 rounded-full ${active === value.title? "bg-orange-500":"bg-white"} ring-2 ring-white`} />
+										<div className={`lg:w-5 lg:h-5 w-2 h-2 md:w-4 md:h-4 md:right-1.5 md:bottom-2 relative lg:bottom-2 lg:right-3 right-[3px] bottom-1 rounded-full ${active === value.title? "bg-orange-500":"bg-white"} ring-2 ring-white`} />
 									</div>
 								);
 							})}
@@ -77,14 +75,13 @@ const Exprience: React.FC = () => {
 							</div> */}
 						</div>
 						<div
-							className="flex flex-col justify-around h-96 font-roboto"
-							style={{ height: "30rem" }}
+							className="flex lg:flex-col justify-around lg:h-[30rem] font-roboto"
 						>
 							{Object.entries(experience).map(([key, value]) => {
 								return (
 									<div
 										key={key}
-										className={`cursor-pointer ${active === value.title && "text-orange-500"} text-3xl hover:scale-105`}
+										className={`cursor-pointer ${active === value.title && "text-orange-500"} md:text-xl lg:text-xl xl:text-3xl hover:scale-105 w-1/4 xl:w-fit text-center`}
 										onClick={() => {
                                             changeActive(value.title)
 											setexp(experience[key]);
@@ -100,31 +97,25 @@ const Exprience: React.FC = () => {
 							})}
 						</div>
 					</div>
-					<div className="basis-1/2">
+					<div className="basis-1/2 mt-5 lg:mt-0">
 						<div
-							className="flex flex-col w-96 rounded-md"
-							style={{
-								width: "35rem",
-								height: "30rem",
-								backgroundColor: "#212121",
-							}}
+							className="flex flex-col lg:w-[35rem] lg:h-[30rem] md:h-[37rem] w-full rounded-md bg-[#212121]"
 						>
 							<div
-								className="basis-3/12 h-14 bg-cover px-4 rounded-t-md"
+								className="basis-3/12 h-14 lg:w-[35rem] bg-cover px-4 rounded-t-md"
 								style={{
 									backgroundImage: "url('images/bg2.jpg')",
-									width: "35rem",
 								}}
 							>
-								<div className="font-roboto text-4xl mt-10">{exp.title}</div>
-								<div className="flex justify-between ms-1">
-									<p className="font-roboto text-lg">{exp.company}</p>
-									<p className="text-lg">{`${exp.duration[0]}-${exp.duration[1]}`}</p>
+								<div className="font-roboto text-2xl md:text-4xl md:mt-10 mt-3">{exp.title}</div>
+								<div className="flex justify-between md:ms-1 mb-1">
+									<p className="font-roboto text-base md:text-xl">{exp.company}</p>
+									<p className="text-base md:text-xl">{`${exp.duration[0]}-${exp.duration[1]}`}</p>
 								</div>
 							</div>
-							<div className="overflow-auto h-52 mt-3">
+							<div className="overflow-auto h-52 md:h-80 lg:h-52  mt-3">
 								<div className=" ms-9">
-									<ul className="list-disc text-md">
+									<ul className="list-disc md:text-xl lg:text-lg text-sm">
 										{exp.work.map((item, index) => {
 											return (
 												<li key={`${index}${item.slice(0, 5)}`}>{item}</li>
@@ -133,12 +124,12 @@ const Exprience: React.FC = () => {
 									</ul>
 								</div>
 							</div>
-							<div className="mt-3 ms-5">
-								<p className="font-roboto text-lg">TechStack</p>
+							<div className="mt-3 ms-5 mb-4">
+								<p className="font-roboto md:text-2xl text-xl lg:text-lg xl:text-2xl ">TechStack</p>
 								<div className="flex flex-wrap gap-5 w-full overflow-y-auto h-14 mt-2">
 									{exp.techStack.map((tech) => {
 										return (
-											<TechTile tech={tech.name} key={tech.name} img={tech.img} link={tech.link} dimensions="11" />
+											<TechTile tech={tech.name} key={tech.name} img={tech.img} link={tech.link} dimensions="14" />
 										);
 									})}
 								</div>

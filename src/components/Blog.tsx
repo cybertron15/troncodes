cccf7752295e1178212f64992ce6f5d07aef28c5
Palash Dhavle <palashdhavle15@gmail.com>
2024-06-11 21:28:02 +0530

    added new blogs

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -5,7 +5,7 @@ import { ArrowUpRight } from "lucide-react";
 const Blog: React.FC = () => {
 	return (
 		<div className="px-5">
-			<div className="md:flex md:justify-between lg:mt-10 md:mt-8 mt-5">
+			<div className="md:flex md:justify-between lg:mt-10 md:mt-8 mt-5 mb-2">
 				<div className="font-roboto md:text-3xl lg:text-5xl sm:text-5xl text-4xl">I Enjoy Writing</div>
 				<a
 					href="https://blog.troncodes.com/"
@@ -21,7 +21,7 @@ const Blog: React.FC = () => {
 					</div>
 				</a>
 			</div>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 space-y-4 md:space-y-0 pt-8 pb-20 w-full">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 space-y-4 md:space-y-0 pt-8 pb-20 w-full h-[32rem] overflow-y-scroll overflow-x-hidden px-2">
 				{blogs.map((blog) => {
 					return (
 							<BlogItem
@@ -31,7 +31,6 @@ const Blog: React.FC = () => {
 							desc={blog.desc}
 							link={blog.link}
 							/>
-					
 					);
 				})}
 			</div>
--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -7,6 +7,54 @@ type Blog<T> = {
 
 const blogs: Blog<string>[] = [
 	{
+		title:"TypeScript Unlocked: Introduction",
+		desc: "In this section we will see what is TypeScript, why do we use it and bit of it's history. Don't worry I won't bore you much I'm just covering this so you get the context why was TypeScript introduced in the first place.",
+		img: "images/blogs/typescript.png",
+		link: "https://blog.troncodes.com/typescript-unlocked-introduction",
+	},
+	{
+		title: "TypeScript Unlocked: Types",
+		desc: "To understand Types we can take example of real world Caste System. In Caste system people are divided into different groups based on the work they do and they are treated differently based on their castes. ",
+		img: "images/blogs/typescript.png",
+		link: "https://blog.troncodes.com/typescript-unlocked-types",
+	},
+	{
+		title:"TypeScript Unlocked: Functions",
+		desc: "Looking at the above image you can see how we do type checking when we write a function. If you don’t specify any type for the function parameter or function return type it will default to type of any which is not recommended when using type script.",
+		img: "images/blogs/typescript.png",
+		link: "https://blog.troncodes.com/typescript-unlocked-functions",
+	},
+	{
+		title: "TypeScript Unlocked: Type Aliases",
+		desc: "Type Aliases allows us to define the structure or shape of the object with the help of type keyword. Lets understand this with an example.",
+		img: "images/blogs/typescript.png",
+		link: "https://blog.troncodes.com/type-aliases",
+	},
+	{
+		title: "TypeScript Unlocked: Readonly, Optional and Type Mixing",
+		desc: "Readonly feature enables us to make properties of the an object readonly. This means we cannot change the value once it has been set, it can only be read.",
+		img: "images/blogs/typescript.png",
+		link: "https://blog.troncodes.com/typescript-unlocked-readonly-optional-and-type-mixing",
+	},
+	{
+		title: "TypeScript Unlocked: Union",
+		desc: "Union enables us to assign two types to a single variable. Sometimes there might arise a situation where we might not know what kind of data will be encountering. In this kind of situation union comes in very handy. ",
+		img: "images/blogs/typescript.png",
+		link: "https://blog.troncodes.com/typescript-unlocked-union",
+	},
+	{
+		title: "TypeScript Unlocked: Tuple",
+		desc: "Tuple is TypeScript Specific thing and we don’t have tuples in JavaScript. Tuples allow us to enforce order of types in an array.",
+		img: "images/blogs/typescript.png",
+		link: "https://blog.troncodes.com/typescript-unlocked-tuple",
+	},
+	{
+		title: "TypeScript Unlocked: Interfaces",
+		desc: "Interfaces works similar to type keyword which we have discussed earlier in the series. Interface allows us to define the structure of the object just like type but there are few differences.",
+		img: "images/blogs/typescript.png",
+		link: "https://blog.troncodes.com/typescript-unlocked-interfaces",
+	},
+	{
 		title: "JSX exposed! What is JSX and how it works",
 		desc: "JSX stands for JavaScript syntax extension. It is a JavaScript extension that allows us to describe React's object tree using a syntax that resembles that of an HTML template",
 		img: "images/blogs/jsx.jpg",
@@ -25,6 +73,13 @@ const blogs: Blog<string>[] = [
 		img: "images/blogs/odometer.png",
 		link: "https://blog.troncodes.com/cool-looking-counter-in-plain-htmlcss-and-js",
 	},
+	{
+		title: "Setup and understand Apollo server in 10 mins",
+		desc: "Apollo Server is a community-driven, open-source GraphQL server that simplifies the process of building and serving GraphQL APIs in JavaScript environments. ",
+		img: "images/blogs/apollo.png",
+		link: "https://blog.troncodes.com/setup-and-understand-apollo-server-in-10-mins",
+	},  
+
 ];
 
 export default blogs;
